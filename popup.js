d8897e3379c9476f6729238e82c8482968ce5d11
Prisha Vadhavkar <prishavadhavkar@gmail.com>
2026-01-10 09:55:18 +0530

    streak updation logic added

--- popup.js
+++ popup.js
@@ -9,6 +9,15 @@ function getToday()
     return today;
 }
 
+function getYesterday()
+{
+    const date = new Date();
+    date.setDate(date.getDate()-1);
+    const yest = date.substring(0, date.indexOf("T"));
+    return yest;
+}
+
+//checks whether streak is already logged today
 chrome.storage.local.get(["streak", "lastSolved"], function(data){
     const streak = data.streak || 0;
     const lastSolved = data.lastSolved;
@@ -19,4 +28,34 @@ chrome.storage.local.get(["streak", "lastSolved"], function(data){
         solved.disabled = true;
         stat.textContent = "Already solved today";
     }
+});
+
+//when solved is clicked
+solved.addEventListener("click", () => {
+    chrome.storage.local.get(["streak", "lastSolved"], (data) => {
+        const streak = data.streak || 0;
+        const lastSolved = data.lastSolved;
+
+        if(lastSolved === getYesterday())
+        {
+            streak+=1;
+        }
+        else
+        {
+            streak=1;
+        }
+        const today = getToday();
+
+        chrome.storage.local.set(
+            {
+                streak: streak,
+                lastSolved: today
+            },
+            () => {
+                streak.textContent = streak;
+                solved.disabled = true;
+                stat.textContent = "Streak Updated!";
+            }
+        );
+    });
 });
